ffcd11d30817085fc69b32d043877b4d8ad3a8a9
It-is-Me49 <christiantroth22@gmail.com>
2020-10-29 18:41:36 -0400

    added new features

--- spaceinvadors.py
+++ spaceinvadors.py
@@ -4,8 +4,8 @@ import pygame
 pygame.init()
 
 # debug settings
-showHitBoxes = True
-showMouse = True
+showHitBoxes = False
+showMouse = False
 
 # actual code
 width = 1536
@@ -15,11 +15,18 @@ window = pygame.display.set_mode((width, height), pygame.RESIZABLE)
 pygame.display.set_caption('Space Invadors')
 images = os.getcwd() + '/Images/'
 sounds = os.getcwd() + '/Sounds/'
+fonts = os.getcwd() + '/Fonts/'
 hearts = pygame.image.load(images + 'hearts.jpg')
 fire = pygame.mixer.Sound(sounds + 'lasershot1.wav')
+fire.set_volume(0.7)
 fire3 = pygame.mixer.Sound(sounds + 'lasershot2.wav')
+fire3.set_volume(0.7)
 explosion = pygame.mixer.Sound(sounds + 'explosion.wav')
+siren = pygame.mixer.Sound(sounds + 'siren.wav')
+siren.set_volume(0.2)
 pygame.mixer.music.load(sounds + 'Fade.mp3')
+pygame.mixer.music.set_volume(0.7)
+pygame.mixer.music.play(-1)
 clock = pygame.time.Clock()
 
 
@@ -35,9 +42,19 @@ class spaceships:
 
     def draw(self, window):
         window.blit(self.image, (self.x, self.y))
+        self.hitbox = (self.x + 16, self.y + 6, self.w - 32, self.h - 15)
         if showHitBoxes is True:
-            self.hitbox = (self.x + 16, self.y + 6, self.w - 32, self.h - 15)
             pygame.draw.rect(window, (255, 0, 0), self.hitbox, 1)
+
+    def blink(self, window):
+        siren.play()
+        for i in range(3):
+            window.fill((0, 0, 0), self.hitbox)
+            pygame.display.update()
+            pygame.time.delay(200)
+            self.draw(window)
+            pygame.display.update()
+            pygame.time.delay(200)
 
 
 class lasers:
@@ -52,14 +69,12 @@ class lasers:
 
     def draw(self, window):
         window.blit(self.image, (self.x, self.y))
+        self.hitbox = (self.x + 1, self.y, self.w - 2, self.h - 1)
         if showHitBoxes is True:
-            self.hitbox = (self.x + 1, self.y, self.w - 2, self.h - 1)
             pygame.draw.rect(window, (255, 0, 0), self.hitbox, 1)
 
 
 class alien1:
-    explode = pygame.image.load(images + 'explosion.png')
-
     def __init__(self, x, y):
         self.w = 50
         self.h = 50
@@ -68,29 +83,31 @@ class alien1:
         self.vel = 1
         self.direction = 'right'
         self.image = pygame.image.load(images + 'alien1.jpg')
+        self.explode = pygame.image.load(images + 'destroy1.jpg')
         self.hitbox = (self.x + 15, self.y + 21, self.w - 30, self.h - 42)
 
     def draw(self, window):
         window.blit(self.image, (self.x, self.y))
+        self.hitbox = (self.x, self.y + 5, self.w, self.h - 11)
         if showHitBoxes is True:
-            self.hitbox = (self.x, self.y + 5, self.w, self.h - 11)
             pygame.draw.rect(window, (255, 0, 0), self.hitbox, 1)
 
     def destroy(self, window, lst):
         explosion.play()
-        self.image = alien1.explode
+        self.image = self.explode
 
 
 class alien2(alien1):
     def __init__(self, x, y):
         super().__init__(x, y)
         self.image = pygame.image.load(images + 'alien2.jpg')
+        self.explode = pygame.image.load(images + 'destroy2.jpg')
         self.hitbox = (self.x + 16, self.y + 16, self.w - 32, self.h - 32)
 
     def draw(self, window):
         window.blit(self.image, (self.x, self.y))
+        self.hitbox = (self.x, self.y, self.w, self.h)
         if showHitBoxes is True:
-            self.hitbox = (self.x, self.y, self.w, self.h)
             pygame.draw.rect(window, (255, 0, 0), self.hitbox, 1)
 
 
@@ -98,12 +115,13 @@ class alien3(alien1):
     def __init__(self, x, y):
         super().__init__(x, y)
         self.image = pygame.image.load(images + 'alien3.png')
+        self.explode = pygame.image.load(images + 'destroy3.jpg')
         self.hitbox = (self.x + 18, self.y + 25, self.w - 36, self.h - 39)
 
     def draw(self, window):
         window.blit(self.image, (self.x, self.y))
+        self.hitbox = (self.x + 4, self.y + 3, self.w - 8, self.h - 8)
         if showHitBoxes is True:
-            self.hitbox = (self.x + 4, self.y + 3, self.w - 8, self.h - 8)
             pygame.draw.rect(window, (255, 0, 0), self.hitbox, 1)
 
 
@@ -113,12 +131,13 @@ class alien4(alien1):
         self.w = 60
         self.h = 60
         self.image = pygame.image.load(images + 'alien4.jpg')
+        self.explode = pygame.image.load(images + 'destroy4.jpg')
         self.hitbox = (self.x + 5, self.y + 16, self.w - 10, self.h - 32)
 
     def draw(self, window):
         window.blit(self.image, (self.x, self.y))
+        self.hitbox = (self.x, self.y + 9, self.w, self.h - 17)
         if showHitBoxes is True:
-            self.hitbox = (self.x, self.y + 9, self.w, self.h - 17)
             pygame.draw.rect(window, (255, 0, 0), self.hitbox, 1)
 
 
@@ -128,12 +147,13 @@ class alien5(alien1):
         self.w = 70
         self.h = 70
         self.image = pygame.image.load(images + 'alien5.jpg')
+        self.explode = pygame.image.load(images + 'destroy5.jpg')
         self.hitbox = (self.x + 2, self.y + 24, self.w - 4, self.h - 48)
 
     def draw(self, window):
         window.blit(self.image, (self.x, self.y))
+        self.hitbox = (self.x, self.y + 18, self.w, self.h - 36)
         if showHitBoxes is True:
-            self.hitbox = (self.x, self.y + 18, self.w, self.h - 36)
             pygame.draw.rect(window, (255, 0, 0), self.hitbox, 1)
 
 
@@ -147,11 +167,27 @@ class alien5(alien1):
 def drawWindow(window, loop):
     clock.tick(144)
     window.fill((0, 0, 0))
+    if lives == 0:
+        font = pygame.font.Font(fonts + 'VCR_OSD_MONO_1.001.ttf', 200)
+        text = font.render('YOU LOST!', True, (0, 255, 0))
+        window.blit(text, (int((width - text.get_width()) / 2), int((height - text.get_height()) / 2)))
+        pygame.display.update()
+        pygame.time.delay(5000)
+        pygame.quit()
+        quit()
+    elif aliens == []:
+        font = pygame.font.Font(fonts + 'VCR_OSD_MONO_1.001.ttf', 200)
+        text = font.render('YOU WIN!', True, (0, 255, 0))
+        window.blit(text, (int((width - text.get_width()) / 2), int((height - text.get_height()) / 2)))
+        pygame.display.update()
+        pygame.time.delay(5000)
+        pygame.quit()
+        quit()
+    spaceship.draw(window)
     for alien in aliens:
         alien.draw(window)
-        if alien.image == alien1.explode and loop == 20:
+        if alien.image == alien.explode and loop == 30:
             aliens.remove(alien)
-    spaceship.draw(window)
     # aliens1.draw(window)
     # aliens2.draw(window)
     # aliens3.draw(window)
@@ -205,6 +241,14 @@ while run:
                     r_shots.remove(laser)
                     loop = 0
 
+    for alien in aliens:
+        if alien.hitbox[0] < spaceship.hitbox[0] + spaceship.hitbox[2] and alien.hitbox[0] + alien.hitbox[2] > spaceship.hitbox[0]:
+            if alien.hitbox[1] < spaceship.hitbox[1] + spaceship.hitbox[3] and alien.hitbox[1] + alien.hitbox[3] > spaceship.hitbox[1]:
+                aliens.remove(alien)
+                spaceship.blink(window)
+                lives -= 1
+        # add if alien passes the spaceship
+
     for laser in r_shots:
         if laser.y >= 0:
             laser.y -= laser.vel
@@ -221,7 +265,7 @@ while run:
             if max(a.x for a in aliens) >= width - 150 - alien.w and alien == aliens[-1]:
                 change = True
         elif change:
-            alien.y += 40
+            alien.y += 50
             if alien.direction == 'right':
                 alien.direction = 'left'
             else:
@@ -264,8 +308,7 @@ while run:
     if (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and spaceship.x <= width - 100 - spaceship.w:
         spaceship.x += spaceship.vel
     if keys[pygame.K_m]:
-        pygame.mixer.music.play(-1)
-        # pygame.mixer.music.stop()
+        pygame.mixer.music.stop()
 
     drawWindow(window, loop)
     loop += 1
